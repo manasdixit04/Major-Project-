05cd5013dfeb62ba455417e24d4f6548b704066c
manasdixit04 <manasdixit904@gmail.com>
2026-02-19 15:16:16 +0530

    utils folder added

---
+++ server/src/utils/ApiError.js
@@ -0,0 +1,24 @@
+class ApiError extends Error{
+    constructor(
+        statusCode,
+        message = "Something went wrong",
+        error = "",
+        stack = []
+    ) {
+        super(message);
+        this.statusCode = statusCode;
+        this.error = error
+        this.message = message;
+        this.data = null
+        this.success = false
+
+        if (stack) {
+            this.stack = stack
+        }
+        else {
+            Error.captureStackTrace(this, this.constructor)
+        }
+    }
+}
+
+export { ApiError }
