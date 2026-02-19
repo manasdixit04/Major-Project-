c9d57f890da1650948502521d3ea2f68d3f0904e
manasdixit04 <manasdixit904@gmail.com>
2026-02-19 14:33:58 +0530

    array created

---
+++ client/src/components/index.js
@@ -0,0 +1,8 @@
+import Input from "./Input.jsx";
+import Button from "../components/Button.jsx";
+import Select from "./Select.jsx";
+import Navbar from "./Navbar.jsx";
+import Card from "./Card.jsx";
+import TeacherCards from "./TeacherCards.jsx";
+
+export { Input, Button, Select, Navbar, Card, TeacherCards };
